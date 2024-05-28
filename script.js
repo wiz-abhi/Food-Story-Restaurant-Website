ea4c728d55119265d1f8c6d8705e0ac4875dcc9b
Wiz Abhi <abhishekg8318@gmail.com>
2024-05-28 20:14:39 +0530

    added theme

---
+++ script.js
@@ -0,0 +1,19 @@
+let varu=document.querySelector("div.outt");
+let btn=document.querySelector("#btn");
+let currMode="light";
+btn.addEventListener("click", () => {
+
+    if(currMode=="light")
+    { 
+    currMode="dark";
+    varu.classList.remove("out");
+    varu.classList.add("out2");
+    }
+else {
+    currMode="light";
+    varu.classList.remove("out2");
+    varu.classList.add("out");
+}
+
+console.log(currMode);
+})
